7263cc92de7c55088326a575ea5d3a92f8628fd0
Mishkin Faustini <mishkinf@hotmail.com>
2016-11-16 02:18:43 -0800

    cleaning up the app state section. providing more guidance

--- src/stores/app-state.js
+++ src/stores/app-state.js
@@ -1,21 +1,26 @@
 import {observable, autorun, isObservable} from 'mobx';
 import {LocalStoreAdapter, RestApiStoreAdapter, RegisterNoun } from 'mobx-api';
+import { API_HOST } from '../config/vars';
 
-const apiHost = 'http://localhost:3001/api';
+// 1. Create an observable store with your nouns
 const store = observable({
     isFakeApi: true,
-    articles: { },
-    randomstuff: { }
+    articles: {},
+    randomstuff: {}
 });
 
+// 2. Register your nouns and attach a mobx-api adapter to the noun
 RegisterNoun('articles', store, new LocalStoreAdapter());
 RegisterNoun('randomstuff', store, new LocalStoreAdapter());
 
 store.setFakeApi = function(useFakeApi) {
     store.isFakeApi = useFakeApi;
-    
-    useFakeApi ? RegisterNoun('articles', store, new LocalStoreAdapter()) :
-                    RegisterNoun('articles', store, new RestApiStoreAdapter(apiHost)); 
+
+    if(useFakeApi) {
+      RegisterNoun('articles', store, new LocalStoreAdapter());
+    } else {
+      RegisterNoun('articles', store, new RestApiStoreAdapter(API_HOST, 'article'));
+    }
 };
 
 export default store;
